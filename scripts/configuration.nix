c25c1be7198a5050d656cd2895607faefe701d05
Gaju Bhat <gaju@gajubhat.com>
2022-09-01 19:53:42 -0700

    Switch to systemd-boot EFI boot loader

--- scripts/configuration.nix
+++ scripts/configuration.nix
@@ -9,10 +9,9 @@
       ./custom-configuration.nix
     ];
 
-  # Use the GRUB 2 boot loader.
-  boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
-  boot.loader.grub.device = "/dev/nvme0n1";
+  # Use the systemd-boot EFI boot loader
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
 
   # remove the fsck that runs at startup. It will always fail to run, stopping
   # your boot until you press *.
